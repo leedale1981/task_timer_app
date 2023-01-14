2e648617a40572c9c1349db8dbc57353cc7a35d6
Lee Dale <lee.jdale@gmail.com>
2023-01-14 19:38:53 +0000

    Update

--- lib/screens/task_list_screen.dart
+++ lib/screens/task_list_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:task_timer/shared/menu_bottom.dart';
+import 'package:task_timer/models/task.dart';
 
-class TaskListScreen extends StatelessWidget {
+class TaskListScreen extends StatefulWidget {
   const TaskListScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  List tasks = [Task('Task 1'), Task('Task 2')];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Tasks')),
-      bottomNavigationBar: const MenuBottom()
+      bottomNavigationBar: const MenuBottom(),
+      body: ListView(children: tasks.map((task) =>
+          TextButton(onPressed: () {}, child: Text(task.name))).toList()
+      )
     );
   }
 }
